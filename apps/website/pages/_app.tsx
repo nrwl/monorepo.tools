d988015a83893311e23d094bf345193686311efe
ben <3447705+bcabanes@users.noreply.github.com>
2021-12-23 12:01:22 -0500

    feat(core): add social metas

--- apps/website/pages/_app.tsx
+++ apps/website/pages/_app.tsx
@@ -6,17 +6,40 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
+        <meta name="robots" content="index,follow" />
+        <meta name="googlebot" content="index,follow" />
+        <meta name="twitter:card" content="summary" />
+        <meta
+          name="twitter:image"
+          content="https://monorepo.tools/images/monorepo.png"
         />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
-          rel="stylesheet"
+        <meta name="twitter:site" content="@monorepotools" />
+        <meta name="twitter:creator" content="@nrwl_io" />
+        <meta
+          name="description"
+          content="Everything you need to know about monorepos, and the tools to build them."
         />
+        <meta property="og:title" content="Monorepo explained" />
+        <meta
+          property="og:description"
+          content="Everything you need to know about monorepos, and the tools to build them."
+        />
+        <meta property="og:url" content="https://monorepo.tools" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://monorepo.tools/images/monorepo.png"
+        />
+        <meta property="og:image:alt" content="monorepo official logo" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
+        <meta property="og:site_name" content="monorepo.tools" />
         <title>Monorepo explained</title>
+        <meta
+          name="description"
+          content="Everything you need to know about monorepos, and the tools to build them."
+        />
       </Head>
       <main className="monorepo.tools bg-slate-50 dark:bg-slate-800 text-gray-700 dark:text-gray-300 font-display">
         <Component {...pageProps} />
