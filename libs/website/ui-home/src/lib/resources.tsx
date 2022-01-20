d98cc0d071a46815b38e2b00314ff80e57a424f9
ben <3447705+bcabanes@users.noreply.github.com>
2022-01-20 16:01:38 -0500

    feat(core): resource add books section

--- libs/website/ui-home/src/lib/resources.tsx
+++ libs/website/ui-home/src/lib/resources.tsx
@@ -34,7 +34,6 @@ const people: {
     githubLink: 'https://github.com/nrwl/nx',
   },
 ];
-
 const videosPodcasts: { name: string; link: string }[] = [
   {
     name: 'Monorepos - How the Pros Scale Huge Software Projects',
@@ -71,6 +70,26 @@ const articles: { name: string; link: string }[] = [
     link: 'https://dev.to/nx/lerna-yarn-nx-faster-build-times-better-dev-ergonomics-32a6',
   },
 ];
+const books: {
+  name: string;
+  author: string;
+  excerpt: string;
+  link: string;
+  imageUrl: string;
+}[] = [
+  {
+    name: 'Effective React Development With Nx',
+    author: 'Jack Hsu',
+    excerpt:
+      'A lot of successful organizations such as Google, Facebook, Microsoft' +
+      ' -as well as large open source projects such as Babel, Jest, and React- are all ' +
+      'using the monorepo approach to software development. As you will see in this book,' +
+      ' a monorepo approach can save developers from a great deal of' +
+      ' headache and wasted time.',
+    link: '',
+    imageUrl: '/images/react-dev-nx-cover.svg',
+  },
+];
 
 export function Resources() {
   return (
@@ -91,7 +110,7 @@ export function Resources() {
         <div className="max-w-2xl mx-auto">
           <h2
             id="monorepo-videos-podcasts"
-            className="text-3xl tracking-tight font-semiboldtext-gray-800 dark:text-gray-100sm:text-4xl"
+            className="text-3xl tracking-tight font-semibold text-gray-800 dark:text-gray-100sm:text-4xl"
           >
             Monorepo Videos & Podcasts
           </h2>
@@ -122,7 +141,7 @@ export function Resources() {
         <div className="max-w-2xl mx-auto">
           <h2
             id="monorepo-articles"
-            className="text-3xl tracking-tight font-semiboldtext-gray-800 dark:text-gray-100sm:text-4xl"
+            className="text-3xl tracking-tight font-semibold text-gray-800 dark:text-gray-100sm:text-4xl"
           >
             Monorepo articles
           </h2>
@@ -152,8 +171,56 @@ export function Resources() {
       <div className="mt-24 lg:mt-36 relative max-w-lg mx-auto lg:max-w-7xl">
         <div className="max-w-2xl mx-auto">
           <h2
+            id="monorepo-books"
+            className="text-3xl tracking-tight font-semibold text-gray-800 dark:text-gray-100sm:text-4xl"
+          >
+            Monorepo books
+          </h2>
+          <p className="mt-3 text-xl text-gray-700 dark:text-gray-300 sm:mt-4">
+            Here is a curated list of books about monorepos that we think are
+            worth a read.
+          </p>
+          <ul className="mt-12 space-y-12">
+            {books.map((book) => (
+              <li key={book.name} className="relative">
+                <a
+                  href={book.link}
+                  target="_blank"
+                  rel="noreferrer"
+                  title={book.name}
+                >
+                  <div className="space-y-4 sm:grid sm:grid-cols-3 sm:gap-6 sm:space-y-0 lg:gap-8">
+                    <div className="w-1/2 mx-auto sm:w-full sm:h-0 sm:aspect-w-3 sm:aspect-h-4">
+                      <img
+                        loading="lazy"
+                        className="object-cover shadow-lg rounded-lg"
+                        src="/images/react-dev-nx-cover.svg"
+                        alt={book.name + ' book cover'}
+                      />
+                    </div>
+                    <div className="hidden sm:block sm:col-span-2">
+                      <div className="space-y-4 text-gray-700 dark:text-gray-300">
+                        <div className="text-lg leading-6 font-medium space-y-1">
+                          <h3>Effective React Development With Nx</h3>
+                          <p className="text-base font-normal uppercase">
+                            {book.author}
+                          </p>
+                        </div>
+                        <p className="text-lg">{book.excerpt}</p>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="mt-24 lg:mt-36 relative max-w-lg mx-auto lg:max-w-7xl">
+        <div className="max-w-2xl mx-auto">
+          <h2
             id="monorepo-contributors"
-            className="text-3xl tracking-tight font-semiboldtext-gray-800 dark:text-gray-100sm:text-4xl"
+            className="text-3xl tracking-tight font-semibold text-gray-800 dark:text-gray-100sm:text-4xl"
           >
             Contributors
           </h2>
