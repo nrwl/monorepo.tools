d6aaa3629c9a2d10a5d4b88cf7b1dca3e69c7ceb
Ronak Mistry <ronakmistry36@gmail.com>
2022-02-01 02:18:36 -0600

    fix(core): update Rush URL

--- libs/website/ui-home/src/lib/introduction.tsx
+++ libs/website/ui-home/src/lib/introduction.tsx
@@ -105,7 +105,7 @@ export function Introduction() {
                 <a
                   rel="noreferrer"
                   target="_blank"
-                  href="https://github.com/microsoft/rushstack"
+                  href="https://rushstack.io"
                   className="border-b border-yellow-500 hover:bg-yellow-500 hover:text-gray-800 transition hover:rounded"
                 >
                   Rush (by Microsoft)
