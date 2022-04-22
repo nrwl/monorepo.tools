17f41add5ed560a4a85f7b87ff74763d1f8c45e1
ben <3447705+bcabanes@users.noreply.github.com>
2022-04-21 20:42:44 -0400

    chore(core): upgrade nx to 14.0.1

--- apps/website/jest.config.ts
+++ apps/website/jest.config.ts
@@ -1,10 +1,11 @@
 module.exports = {
   displayName: 'website',
-  preset: '../../jest.preset.js',
+
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/website',
+  preset: '../../jest.preset.ts',
 };
--- libs/website/ui-home/jest.config.ts
+++ libs/website/ui-home/jest.config.ts
@@ -1,9 +1,10 @@
 module.exports = {
   displayName: 'website-ui-home',
-  preset: '../../../jest.preset.js',
+
   transform: {
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/libs/website/ui-home',
+  preset: '../../../jest.preset.ts',
 };
