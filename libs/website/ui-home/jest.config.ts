61a5a23ee0cbc175d7582acb434db05349c29dbd
ben <3447705+bcabanes@users.noreply.github.com>
2022-09-01 13:34:46 -0400

    chore(core): upgrade nx to 14.6.2

--- libs/website/ui-home/jest.config.ts
+++ libs/website/ui-home/jest.config.ts
@@ -2,7 +2,7 @@
 export default {
   displayName: 'website-ui-home',
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/libs/website/ui-home',
